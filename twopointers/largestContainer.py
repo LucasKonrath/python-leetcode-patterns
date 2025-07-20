294f351fb7934aa7e3b55aca7c1bbc051f0843a6
LucasKonrath <lucaskdamaceno@gmail.com>
2025-07-20 14:54:46 -0300

    add two-pointer solution for largest container problem

--- twopointers/largestContainer.py
+++ twopointers/largestContainer.py
@@ -9,3 +9,15 @@ def largest_container_brute_force(heights: List[int]) -> int:
             water = min(heights[i], heights[j]) * (j - i)
             max_water = max(max_water, water)
     return max_water
+
+def largest_container_two_pointers(heights: List[int]) -> int:
+    left, right = 0, len(heights) - 1
+    max_water = 0
+    while left < right:
+        water = min(heights[left], heights[right]) * (right - left)
+        max_water = max(max_water, water)
+        if heights[left] < heights[right]:
+            left += 1
+        else:
+            right -= 1
+    return max_water
