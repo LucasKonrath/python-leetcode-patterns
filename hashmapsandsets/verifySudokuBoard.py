b2827aa731fbefff8192e1f3ffa595c6f78129e2
LucasKonrath <lucaskdamaceno@gmail.com>
2025-07-20 16:39:31 -0300

    fix: correct subgrid_sets initialization in verify_sudoku_board function

--- hashmapsandsets/verifySudokuBoard.py
+++ hashmapsandsets/verifySudokuBoard.py
@@ -4,7 +4,7 @@ from typing import List
 def verify_sudoku_board(board: List[List[int]]) -> bool:
     row_sets = [set() for _ in range(9)]
     column_sets = [set() for _ in range(9)]
-    subgrid_sets = [set() for _ in range(3) for _ in range(3)]
+    subgrid_sets = [[set() for _ in range(3)] for _ in range(3)]
     for r in range(9):
         for c in range(9):
             num = board[r][c]
