5c9f20b6168653d88a92c10f77af3c60ef5f9f15
LucasKonrath <lucaskdamaceno@gmail.com>
2025-07-20 14:53:07 -0300

    add brute force solution to find the largest container of water

---
+++ twopointers/largestContainer.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def largest_container_brute_force(heights: List[int]) -> int:
+    n = len(heights)
+    max_water = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            water = min(heights[i], heights[j]) * (j - i)
+            max_water = max(max_water, water)
+    return max_water
