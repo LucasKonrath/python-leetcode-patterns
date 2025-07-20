986fe96ff5b351a56a2856a4ebbd48e60c716e45
LucasKonrath <lucaskdamaceno@gmail.com>
2025-07-20 14:20:29 -0300

    add unit tests for pairSumSorted function in test_pairSumSorted.py

--- twopointers/test_pairSumSorted.py
+++ twopointers/test_pairSumSorted.py
@@ -1,5 +1,5 @@
 import unittest
-from pairSumSorted import bruteForce
+from pairSumSorted import bruteForce, pairSumSorted
 
 class TestPairSumSorted(unittest.TestCase):
     def test_found_pair(self):
@@ -18,6 +18,20 @@ class TestPairSumSorted(unittest.TestCase):
     def test_single_element(self):
         self.assertEqual(bruteForce([5], 5), [])
 
+    def test_pairSumSorted_found_pair(self):
+        self.assertEqual(pairSumSorted([1, 2, 3, 4, 6], 6), [1, 3])
+
+    def test_pairSumSorted_no_pair(self):
+        self.assertEqual(pairSumSorted([1, 2, 3], 7), [])
+
+    def test_pairSumSorted_multiple_pairs(self):
+        self.assertEqual(pairSumSorted([1, 2, 3, 4, 5], 5), [0, 3])
+
+    def test_pairSumSorted_empty_list(self):
+        self.assertEqual(pairSumSorted([], 5), [])
+
+    def test_pairSumSorted_single_element(self):
+        self.assertEqual(pairSumSorted([5], 5), [])
+
 if __name__ == "__main__":
     unittest.main()
-
