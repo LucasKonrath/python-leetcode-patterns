75cb7db8f3199650125183b9467561a77642dce9
LucasKonrath <lucaskdamaceno@gmail.com>
2025-07-20 14:55:26 -0300

    add unit tests for two-pointer solution in largest container problem

--- twopointers/test_largestContainer.py
+++ twopointers/test_largestContainer.py
@@ -1,5 +1,5 @@
 import unittest
-from twopointers.largestContainer import largest_container_brute_force
+from twopointers.largestContainer import largest_container_brute_force, largest_container_two_pointers
 
 class TestLargestContainer(unittest.TestCase):
     def test_example(self):
@@ -23,6 +23,26 @@ class TestLargestContainer(unittest.TestCase):
     def test_increasing_heights(self):
         self.assertEqual(largest_container_brute_force([1,2,3,4,5]), 6)
 
+    def test_two_pointers_example(self):
+        self.assertEqual(largest_container_two_pointers([1,8,6,2,5,4,8,3,7]), 49)
+
+    def test_two_pointers_empty(self):
+        self.assertEqual(largest_container_two_pointers([]), 0)
+
+    def test_two_pointers_single_element(self):
+        self.assertEqual(largest_container_two_pointers([5]), 0)
+
+    def test_two_pointers_two_elements(self):
+        self.assertEqual(largest_container_two_pointers([1, 1]), 1)
+
+    def test_two_pointers_all_same_height(self):
+        self.assertEqual(largest_container_two_pointers([3,3,3,3]), 9)
+
+    def test_two_pointers_decreasing_heights(self):
+        self.assertEqual(largest_container_two_pointers([5,4,3,2,1]), 6)
+
+    def test_two_pointers_increasing_heights(self):
+        self.assertEqual(largest_container_two_pointers([1,2,3,4,5]), 6)
+
 if __name__ == "__main__":
     unittest.main()
-
