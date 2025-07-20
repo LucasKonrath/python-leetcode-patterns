fdf1f035a4e0c156fbc3d6522338c0138af44beb
LucasKonrath <lucaskdamaceno@gmail.com>
2025-07-20 15:20:43 -0300

    add: implement pair_sum_two_unsorted_one_pass function for optimized pairing

--- hashmapsandsets/pairSumTwoUnsorted.py
+++ hashmapsandsets/pairSumTwoUnsorted.py
@@ -10,3 +10,12 @@ def pair_sum_two_unsorted_two_pass(nums: List[int], target: int) -> List[int]:
         if complement in num_map and num_map[complement] != i:
             return [i, num_map[complement]]
     return []
+
+def pair_sum_two_unsorted_one_pass(nums: List[int], target: int) -> List[int]:
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []
