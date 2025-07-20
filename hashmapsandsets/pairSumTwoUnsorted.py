95c82e8426ab00e12e2d9b205a34d9053a3189fc
LucasKonrath <lucaskdamaceno@gmail.com>
2025-07-20 15:20:05 -0300

    refactor: optimize pair_sum_two_unsorted_two_pass function to use a dictionary for index mapping

--- hashmapsandsets/pairSumTwoUnsorted.py
+++ hashmapsandsets/pairSumTwoUnsorted.py
@@ -2,11 +2,11 @@ from typing import List
 
 
 def pair_sum_two_unsorted_two_pass(nums: List[int], target: int) -> List[int]:
-    num_map = []
+    num_map = {}
     for i, num in enumerate(nums):
-        num_map[i] = num
+        num_map[num] = i
     for i, num in enumerate(nums):
         complement = target - num
-        if complement in num_map:
-            return [i, num_map.index(complement)]
+        if complement in num_map and num_map[complement] != i:
+            return [i, num_map[complement]]
     return []
